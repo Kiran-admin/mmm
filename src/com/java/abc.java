56a06045bd9fe0c6cd9a5a8dffc46cf3f55a2858
Kiran Bagul <kiran.bagul@cog>
2017-01-20 10:52:09 +0530

    first commit

---
+++ src/com/java/abc.java
@@ -0,0 +1,25 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class abc {
+
+	public static void main(String[] args) {
+
+		
+		Map<String,String> myMap = new ConcurrentHashMap<String,String>();
+		myMap.put("1", "1");
+		myMap.put("2", "2");
+		myMap.put("3", "3");
+
+	
+		}
+		   
+}
+}
